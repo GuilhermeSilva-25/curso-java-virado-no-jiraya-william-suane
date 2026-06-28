9b006ca1d2c102ea003695429fa22c9edcdac6f9
Guilherme Silva <guime.silva333@gmail.com>
2026-06-28 14:35:56 -0300

    feat: adiciona estrutura de pacotes e classe inicial do sistema (Aula 08)

---
+++ maratona-java/src/br/com/techbank/core/SystemStartup.java
@@ -0,0 +1,7 @@
+package br.com.techbank.core;
+
+public class SystemStartup {
+    public static void main(String[] args) {
+        System.out.println("Inicializando núcleo do sistema TechBank...");
+    }
+}
