cc6536b538e06beb6dd641a312f3aedcdd99f747
Guilherme Silva <guime.silva333@gmail.com>
2026-06-28 12:40:28 -0300

    feat: adiciona exercício de relatório de status (Aula 07)

---
+++ maratona-java/src/StatusReport.java
@@ -0,0 +1,7 @@
+public class StatusReport {
+    public static void main(String[] args) {
+        System.out.println("Servidor 01: Online");
+        System.out.println("Banco de Dados: Online");
+        System.out.println("Avisos: 0");
+    }
+}
