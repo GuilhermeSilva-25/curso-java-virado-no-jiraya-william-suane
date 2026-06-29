dedc3a5a7106480ed5aaad1fdb9952f0ce2b1947
Guilherme Silva <guime.silva333@gmail.com>
2026-06-29 15:56:37 -0300

    feat: adiciona script de emissão de crachá provisório (Aula 010)

---
+++ maratona-java/src/br/com/techbank/core/BadgeIssuance.java
@@ -0,0 +1,12 @@
+package br.com.techbank.core;
+
+public class BadgeIssuance {
+    public static void main(String[] args) {
+        int employeeId = 33;
+        int yearOfHiring = 2024;
+
+        System.out.println("--- CRACHÁ PROVISÓRIO ---");
+        System.out.printf("Funcionário ID: %d%n", employeeId);
+        System.out.printf("Contratado no ano: %d%n", yearOfHiring);
+    }
+}
